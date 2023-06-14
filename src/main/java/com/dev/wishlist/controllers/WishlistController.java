1c01ef1fd3131b6a751336c341a8f607513f5e50
vitorcvargas <vitorcvargas@gmail.com>
2023-06-14 15:25:22 -0300

    Created controller

---
+++ src/main/java/com/dev/wishlist/controllers/WishlistController.java
@@ -0,0 +1,30 @@
+package com.dev.wishlist.controllers;
+
+import com.dev.wishlist.models.Product;
+import com.dev.wishlist.services.WishlistService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/wishlist")
+public class WishlistController {
+
+    private final Logger logger = LoggerFactory.getLogger(WishlistController.class);
+    private final WishlistService wishlistService;
+
+    public WishlistController(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addToWishlist(@RequestParam final Long userId, @RequestBody final Product product) {
+
+        wishlistService.addToWishlist(product, userId);
+
+        return ResponseEntity.status(CREATED).body("Product added to wishlist");
+    }
+}
