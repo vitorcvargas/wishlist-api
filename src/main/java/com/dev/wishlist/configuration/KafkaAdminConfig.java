c00d3957b2ca47bc7151912de7fe4a2269e80ffd
vitorcvargas <vitorcvargas@gmail.com>
2023-06-15 11:15:45 -0300

    Added kafka to the project

---
+++ src/main/java/com/dev/wishlist/configuration/KafkaAdminConfig.java
@@ -0,0 +1,41 @@
+package com.dev.wishlist.configuration;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Configuration
+public class KafkaAdminConfig {
+
+    @Value("${spring.kafka.topics}")
+    private List<String> topics;
+    private final KafkaProperties properties;
+
+    public KafkaAdminConfig(KafkaProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics newTopics(KafkaAdmin kafkaAdmin) {
+        KafkaAdmin.NewTopics newTopics = new KafkaAdmin.NewTopics(
+                topics.stream().map(topic -> TopicBuilder.name(topic).partitions(2).build())
+                        .toArray(NewTopic[]::new));
+
+        return newTopics;
+    }
+}
---
+++ src/main/java/com/dev/wishlist/configuration/KafkaProducerConfig.java
@@ -0,0 +1,42 @@
+package com.dev.wishlist.configuration;
+
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+    private final KafkaProperties kafkaProperties;
+
+    @Autowired
+    public KafkaProducerConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> jsonProducerFactory() {
+
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configs, new StringSerializer(), new JsonSerializer());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Serializable> jsonKafkaTemplate(ProducerFactory jsonProducerFactory) {
+        return new KafkaTemplate<>(jsonProducerFactory);
+    }
+}
