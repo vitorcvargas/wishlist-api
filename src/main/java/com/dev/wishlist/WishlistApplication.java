9aa9528fa9786b9866b26aabc2cf0642a4494459
vitorcvargas <vitorcvargas@gmail.com>
2023-06-16 09:21:35 -0300

    Enabling mongock

--- src/main/java/com/dev/wishlist/WishlistApplication.java
+++ src/main/java/com/dev/wishlist/WishlistApplication.java
@@ -1,9 +1,11 @@
 package com.dev.wishlist;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongock
 public class WishlistApplication {
 
     public static void main(String[] args) {
