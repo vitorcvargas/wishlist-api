3f1ce774292b4984d8a61c83d019dcd376362b7b
vitorcvargas <vitorcvargas@gmail.com>
2023-06-16 13:34:32 -0300

    Created unit tests for findProductsInWishlist method

--- src/test/java/com/dev/wishlist/services/WishlistServiceTest.java
+++ src/test/java/com/dev/wishlist/services/WishlistServiceTest.java
@@ -1,8 +1,13 @@
 package com.dev.wishlist.services;
 
+import com.dev.wishlist.dtos.WishlistResponse;
 import com.dev.wishlist.exceptions.BadRequestException;
+import com.dev.wishlist.exceptions.NotFoundException;
 import com.dev.wishlist.models.Product;
+import com.dev.wishlist.models.ProductCatalog;
+import com.dev.wishlist.models.ProductProjection;
 import com.dev.wishlist.models.Wishlist;
+import com.dev.wishlist.repositories.ProductCatalogRepository;
 import com.dev.wishlist.repositories.WishlistRepository;
 import com.dev.wishlist.testutils.ProductCreator;
 import org.junit.jupiter.api.DisplayName;
@@ -34,12 +39,20 @@ public class WishlistServiceTest {
     @Mock
     WishlistRepository wishlistRepository;
 
+    @Mock
+    ProductCatalogRepository productCatalogRepository;
+
+    @Mock
+    WishlistNotifier notifier;
+
     @Test
     @DisplayName("Should add product to wishlist")
     void shouldAddProductToWishlist() {
 
         final Long userId = 1L;
         Product product = ProductCreator.createSingleProduct();
+
+        doNothing().when(notifier).notify(anyLong(), anyLong());
 
         assertDoesNotThrow(() -> wishlistService.addToWishlist(product, userId));
         verify(wishlistRepository, times(1)).findByUserId(userId);
@@ -76,6 +89,44 @@ public class WishlistServiceTest {
         BadRequestException badRequestException = assertThrows(BadRequestException.class, () -> wishlistService.addToWishlist(product, userId));
         assertThat(badRequestException.getMessage()).isEqualTo("Product already added to wishlist.");
         verify(wishlistRepository, times(1)).findByUserId(userId);
+    }
+
+    @Test
+    @DisplayName("Should find a product by its name")
+    void shouldFindAProductByItsName() {
+
+        final Long userId = 1L;
+        Product product = createSingleProduct();
+        ProductProjection projection = new ProductProjection();
+        ProductCatalog productCatalog = new ProductCatalog(1L, "Nike", "Nice shoes", new BigDecimal("400.00"), "https://somelink");
+
+        projection.setProducts(List.of(product));
+
+        when(wishlistRepository.findAllProductsByUserIdAndSearchInput(1L, "ni"))
+                .thenReturn(Optional.of(projection));
+
+        when(productCatalogRepository.findAllById(anyList()))
+                .thenReturn(List.of(productCatalog));
+
+        WishlistResponse wishlistResponse = assertDoesNotThrow(() -> wishlistService.findProductsInWishlist(userId, "ni"));
+
+        assertThat(wishlistResponse.getUserId()).isEqualTo(userId);
+        assertThat(wishlistResponse.getProducts()).isEqualTo(List.of(productCatalog));
+
+        verify(wishlistRepository, times(1)).findAllProductsByUserIdAndSearchInput(userId, "ni");
+        verify(productCatalogRepository, times(1)).findAllById(anyList());
+    }
+
+    @Test
+    @DisplayName("Should not find a product by its name and throw exception")
+    void shouldNotFindAProductByItsNameAndThrowException() {
+
+        when(wishlistRepository.findAllProductsByUserIdAndSearchInput(1L, "ni"))
+                .thenReturn(Optional.empty());
+
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () -> wishlistService.findProductsInWishlist(1L, "ni"));
+        assertThat(notFoundException.getMessage()).isEqualTo("Product not found with search input: ni");
+        verify(wishlistRepository, times(1)).findAllProductsByUserIdAndSearchInput(1L, "ni");
     }
 
 }
